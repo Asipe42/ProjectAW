ff71fbb67d82202b4f666665135ee80e6336ca51
Modin <eom3658@gmail.com>
2025-12-11 00:20:17 +0900

    feat: PlayerMovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : NetworkBehaviour
+    {
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner) 
+            {
+                
+            }
+        }
+        
+        private void Update()
+        {
+            if (!IsOwner) 
+                return;
+            
+            var horizontalInput = Input.GetAxis("Horizontal");
+            var verticalInput = Input.GetAxis("Vertical");
+            var moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+            if (moveDirection.magnitude > 1f)
+            {
+                moveDirection.Normalize();
+            }
+
+            if (moveDirection.magnitude > 0)
+            {
+                RequestMovementServerRpc(moveDirection * Time.deltaTime);
+            }
+        }
+        
+        [ServerRpc]
+        private void RequestMovementServerRpc(Vector3 movement)
+        {
+            transform.position += movement;
+        }
+    }
+}
