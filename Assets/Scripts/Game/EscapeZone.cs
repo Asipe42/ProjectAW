038028dd431e94b6ca49cf443e59659d9cb6ec5f
Modin <eom3658@gmail.com>
2025-12-27 23:48:50 +0900

    feat: 게임 상태 관리

---
+++ Assets/Scripts/Game/EscapeZone.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Game
+{
+    public class EscapeZone : NetworkBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsServer)
+                return;
+
+            if (other.CompareTag("Player"))
+            {
+                GameManager.Instance.EndGame(isWin: true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : NetworkBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        public NetworkVariable<GameState> CurrentState = new(GameState.Playing);
+
+        private void Awake()
+        {
+            if (Instance == null) 
+                Instance = this;
+            else 
+                Destroy(gameObject);
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            CurrentState.OnValueChanged += OnStateChanged;
+        }
+        
+        public void EndGame(bool isWin)
+        {
+            if (!IsServer) 
+                return;
+            
+            if (CurrentState.Value != GameState.Playing) 
+                return;
+
+            CurrentState.Value = isWin ? GameState.Success : GameState.Fail;
+        }
+
+        private void OnStateChanged(GameState previousValue, GameState newValue)
+        {
+            switch (newValue)
+            {
+                case GameState.Success:
+                    Debug.Log("미션 완료");
+                    break;
+                case GameState.Fail:
+                    Debug.Log("미션 실패");
+                    break;
+            }
+        }
+    }
+}
